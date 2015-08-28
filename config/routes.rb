6abc86de285b2c635c3acb61ca734667eefb0b89
Wyns <pauwains@gmail.com>
2015-08-28 19:01:39 +0300

    added Authorsessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ resources :articles do
 resources :comments
 end
 resources :tags
+ resources :authors
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
